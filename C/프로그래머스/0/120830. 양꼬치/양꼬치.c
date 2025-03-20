a403265aa40648e281b128e1af6d0fff7e6f8102
MinseoKim09 <u01056305988@gmail.com>
2025-03-20 22:50:54 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- C/프로그래머스/0/120830. 양꼬치/양꼬치.c
+++ C/프로그래머스/0/120830. 양꼬치/양꼬치.c
@@ -3,7 +3,5 @@
 #include <stdlib.h>
 
 int solution(int n, int k) {
-    int answer = 0;
-    answer = n * 12000 + k * 2000 - (n/10) * 2000;
-    return answer;
+    return n * 12000 + k * 2000 - (n/10) * 2000;
 }
