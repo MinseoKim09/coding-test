d02ce3ba25cb7f86f31900bf1d5471011054b858
MinseoKim09 <u01056305988@gmail.com>
2025-03-21 16:29:35 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- C/프로그래머스/0/120831. 짝수의 합/짝수의 합.c
+++ C/프로그래머스/0/120831. 짝수의 합/짝수의 합.c
@@ -4,8 +4,8 @@
 
 int solution(int n) {
     int answer = 0;
-    for (int i = 1; i <= n; i++)
-        if (i%2 == 0)
+    for(int i = 1; i<=n; i++)
+        if(i%2 == 0)
             answer+=i;
     return answer;
 }
