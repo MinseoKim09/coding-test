1d0b5fc9f868602d58a932020d4b82783a122192
MinseoKim09 <u01056305988@gmail.com>
2025-07-02 22:36:36 +0900

    [Bronze III] Title: 최댓값, Time: 204 ms, Memory: 18560 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int max = -1;
+        int row = 0;
+        int col = 0;
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                int num = sc.nextInt();
+                if (num > max) {
+                    max = num;
+                    row = i + 1;
+                    col = j + 1;
+                }
+            }
+        }
+        System.out.println(max);
+        System.out.println(row + " " + col);
+    }
+}
