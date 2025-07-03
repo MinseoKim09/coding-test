f465549ea0041abe377d5bfb226dd9402fddb07f
MinseoKim09 <u01056305988@gmail.com>
2025-07-03 19:28:55 +0900

    [Bronze III] Title: 대지, Time: 848 ms, Memory: 109588 KB -BaekjoonHub

---
+++ Java/백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int maxY = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            if (x < minX) minX = x;
+            if (x > maxX) maxX = x;
+            if (y < minY) minY = y;
+            if (y > maxY) maxY = y;
+        }
+        int w = maxX - minX;
+        int h = maxY - minY;
+        System.out.println(w * h);
+    }
+}
