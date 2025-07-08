1d449d8f85664d9311584cccf433535b52fc7eca
MinseoKim09 <u01056305988@gmail.com>
2025-07-08 14:52:59 +0900

    [Bronze II] Title: 16진수, Time: 192 ms, Memory: 17600 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String h = sc.next();
+        int d = Integer.parseInt(h, 16);
+        System.out.println(d);
+    }
+}
